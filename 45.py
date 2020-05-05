884231f5e5f156195162b3b20ef3f3e00928827e
BoykoDenis <denya.boyko@gmail.com>
2020-05-05 16:41:01 +0200

    Revert "first commit"
    This reverts commit 6b5e7305c7577d5ace482f505ee58c7a026209de.

--- 45.py
+++
@@ -1,6 +0,0 @@
-import torch
-import matplotlib.pyplot as plt
-import numpy as np
-from torchvision import datasets, transforms
-
-training_dataset = datasets.MNIST(root = './data', train = True, download = True)
--- LinearRegresion1.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-w = torch.tensor(5.0, requires_grad = True)
-b = torch.tensor(3.0, requires_grad = True)
-
-def forward(x):
-	y = w*x + b
-	return y
